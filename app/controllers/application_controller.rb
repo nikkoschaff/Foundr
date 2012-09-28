e33703521708c744ea3c600957a045d8fd067428
John J. Glynn IV <jjg1914@rit.edu>
2012-09-28 15:49:55 -0400

    Added Roles controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+    protect_from_forgery
+
+private
+
+    def role_authenticated?
+        ( session.has_key?( :role_id ) and !session[:role_id].nil? )
+    end
+
+    def role_current
+        unless !role_authenticated
+            @role_current ||= Role.find( session[:role_id] )
+        end
+        return @role_current
+    end
+
+    def role_authenticate( name, password )
+        raise RuntimeError if role_authenticated?
+
+        @role_current = Role.authenticate( name, password )
+        session[:role_id] = @role_current.id if @role_current
+        return @role_current
+    end
+
+    def role_unauthenticate
+        raise RuntimeError if !role_authenticated?
+
+        session.delete :role_id
+        @role_current = nil
+        return true
+    end
+
+    def filter_authenticated
+        filter_unauthorized unless role_authenticated?
+    end
+
+    def filter_unauthenticated
+        filter_unauthorized unless !role_authenticated?
+    end
+
+    def filter_unauthorized
+        redirect_to :action => :unauthorized, :controller => :application
+        return false
+    end
+
 end
---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,60 @@
+class RolesController < ApplicationController
+    before_filter :filter_authenticated, :only => [ :index,
+                                                    :new,
+                                                    :show,
+                                                    :edit,
+                                                    :create
+                                                    :update
+                                                    :destroy,
+                                                    :logout ]
+    before_filter :filter_unauthenticated, :only => [ :login,
+                                                      :authenticate,
+                                                      :signup ]
+    before_filter :find_role, :only => [ :show,
+                                         :edit,
+                                         :update,
+                                         :destroy ]
+
+    def index
+        @roles = Role.all
+    end
+
+    def new
+        @role = Roll.new
+    end
+
+    def create
+        @role = Role.create( params[:role] )
+        if @role.save
+            redirect_to :action => :index
+        else
+            render :action => :new
+        end
+    end
+
+    def update
+        if @role.update_attributes( params[:role] )
+            redirect_to :action => :index
+        else
+            render :action => :edit
+        end
+    end
+
+    def destroy
+        @role.destroy
+        redirect_to :action => :index
+    end
+
+    def logout
+        role_unauthenticate
+        redirect_to :action => :login
+    end
+
+    def authenticate
+        if role_authenticate( params[:name], params[:password] )
+            redirect_to :action => :index
+        else
+            render :action => :login
+        end
+    end
+end
