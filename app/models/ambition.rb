e13f53d4912835ddabf207bc3473b8bc8328ae04
John J. Glynn IV <jjg1914@rit.edu>
2012-09-28 12:30:09 -0400

    Revert "Initial commit with scaffold generated User, model generated Ambition, Skill, and employment status.  DB Seeds made for ambition and employment status"
    This reverts commit 373f65136b805f627f691efac74c10ae8436f8f4.

--- app/models/ambition.rb
+++
@@ -1,5 +0,0 @@
-class Ambition < ActiveRecord::Base
-  attr_accessible :name
-  validates_uniqueness_of :name
-  belongs_to :user
-end
--- db/migrate/20120927223054_create_ambitions.rb
+++
@@ -1,9 +0,0 @@
-class CreateAmbitions < ActiveRecord::Migration
-  def change
-    create_table :ambitions do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
