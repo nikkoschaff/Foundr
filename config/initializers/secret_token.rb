9fccba655dd6ab11c7110f33474fc0045c575efb
John J. Glynn IV <jjg1914@rit.edu>
2012-09-28 12:46:16 -0400

    Intial rails commit ( default app ).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foundr::Application.config.secret_token = 'ff413ef4f7f02a40a81b4fb202df2561f95539c3022c54fe1bd1cd6e4539c6a9d9f4f8dcc50701577a9bb8bfd27a9d6a54239087def2aed17541d013335789f4'
