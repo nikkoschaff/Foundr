631608ebbab3f84eca5c1d39c63b2ace08f14911
Nikko Schaff <nikko.schaff@gmail.com>
2012-09-29 16:14:31 -0400

    saving before merge - made style changes to role pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Role.create!(:name => "n12345678",:password => "123456789",:password_confirm => "123456789")
+#Role.create!(:name => "n23456789",:password => "123456789",:password_confirm => "123456789")
+#Role.create!(:name => "n345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "an345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "bn345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "cn345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "dn345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "en345678910",:password => "123456789",:password_confirm => "123456789")
+Role.create!(:name => "fn345678910",:password => "123456789",:password_confirm => "123456789")
