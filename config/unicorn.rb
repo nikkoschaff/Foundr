2e9279e63c0a73d10b159c4e02bb40d1f5990f17
Nikko Schaff <nikko.schaff@gmail.com>
2012-10-10 20:01:51 -0400

    webserver config changes

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/usr/share/nginx/html/Foundr/"
+pid "/usr/share/nginx/html/Foundr/tmp/pids/unicorn.pid"
+stderr_path "/usr/share/nginx/html/Foundr/log/unicorn.log"
+stdout_path "/usr/share/nginx/html/Foundr/log/unicorn.log"
+
+listen "/usr/share/nginx/html/Foundr/unicorn.todo.sock"
+worker_processes 2
+timeout 30
