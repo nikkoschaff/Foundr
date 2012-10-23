070644d08cee06130451dc5c6906afbe1172d87e
Nikko Schaff <nikko.schaff@gmail.com>
2012-10-23 10:57:57 -0400

    Tested - ready for Ryan

--- config/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "/usr/share/nginx/html/Foundr/"
-pid "/usr/share/nginx/html/Foundr/tmp/pids/unicorn.pid"
-stderr_path "/usr/share/nginx/html/Foundr/log/unicorn.log"
-stdout_path "/usr/share/nginx/html/Foundr/log/unicorn.log"
-
-listen "/usr/share/nginx/html/Foundr/unicorn.todo.sock"
-worker_processes 2
-timeout 30
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,3 @@ unless Ambition.find_by_id(3)
 	Ambition.create!(:name => "Join a team")
 	Ambition.create!(:name => "Networking")
 end
-
-# Load admin role
-unless Role.find_by_id(1)
-	Role.create!(:email => "admin@foundrapp.com", :password => "qwertyuiop", :password_confirm => "qwertyuiop")
-end
-
-# Load admin profile
-unless Profile.find_by_id(1)
-	Profile.create!(:name => "admin", :headline => "admin", :about => "admin")
-end
-
-
-@role = Role.find(1)
-@role.build_profile(:name => "admin", :headline => "admin", :about => "admin")
-@role.save!
